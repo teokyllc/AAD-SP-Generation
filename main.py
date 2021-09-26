eb454683b26350a7239b17e054230c4fef494054
Allan Taylor <allan@startmail.com>
2021-09-26 11:38:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import functions as f
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/HealthCheck", methods=["GET"])
-def health_check():
-    return "Works"
-
 @app.route("/CreateSP", methods=["POST"])
 def create_sp():
     app_name = str(request.json.get('app_name', ''))
@@ -15,6 +11,13 @@ def create_sp():
     client_id = sp_secrets["sp-creator-client-id"]
     client_secret = sp_secrets["sp-creator-client-secret"]
     new_sp = f.new_aad_sp(client_id, client_secret, app_name)
-    return new_sp, 200
+    service_principal_name = new_sp["appDisplayName"]
+    aad_app_id = new_sp["appId"]
+    sp_client_id = new_sp["id"]
+    sp_client_secret_data = f.create_aad_sp_credential(sp_client_id)
+    sp_client_secret = sp_client_secret_data["secretText"]
+    sp_client_secret_key_id = sp_client_secret_data["keyId"]
+    f.write_vault_secret(service_principal_name, aad_app_id, sp_client_id, sp_client_secret, sp_client_secret_key_id)
+    return new_sp
 
 app.run(host='0.0.0.0')
